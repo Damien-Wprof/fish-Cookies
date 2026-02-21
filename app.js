6b2b37df0d47881ae8a59ff78f8fd369607ecf6d
damien welch <nomad2142@protonmail.com>
2026-02-21 15:11:22 -0800

    added new location objects and gave custom identifiers to elements

--- app.js
+++ app.js
@@ -3,20 +3,18 @@ let seattle = {
     phone: "555-1212",
     address: "68523 Damienwzhere",
     minCustomers: 23, 
-    maxCustomers: 64,
+    maxCustomers: 65,
     avgSale: 6.3,
     estimates: [],
-    // do it like we did with Geno's age ...
-    // estimate: this.estimates = generateEstimate( this.minCustomers, this.maxCustomers, this.avgSale )
 }
 
 let tokyo = {
- name: "Seattle",
+    name: "Tokyo",
     phone: "555-1313",
     address: "4458 Urmom Ave",
-    minCustomers: 23, 
-    maxCustomers: 64,
-    avgSale: 6.3,
+    minCustomers: 3, 
+    maxCustomers: 24,
+    avgSale: 1.2,
     estimates: [],
 }
 
@@ -24,8 +22,28 @@ let paris = {
  name: "Paris",
     phone: "555-1414",
     address: "123 playground stret",
-    minCustomers: 23, 
-    maxCustomers: 64,
-    avgSale: 6.3,
+    minCustomers: 11, 
+    maxCustomers: 38,
+    avgSale: 3.7,
+    estimates: [],
+}
+
+let brazil = {
+ name: "Brazil",
+    phone: "555-4930",
+    address: "123456789 ave",
+    minCustomers: 20, 
+    maxCustomers: 38,
+    avgSale: 2.3,
+    estimates: [],
+}
+
+let quebec = {
+ name: "Quebec",
+    phone: "555-9036",
+    address: "6416 NE sand DR",
+    minCustomers: 2, 
+    maxCustomers: 16,
+    avgSale: 4.6,
     estimates: [],
 }
